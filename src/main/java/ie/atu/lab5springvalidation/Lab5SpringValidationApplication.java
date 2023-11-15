be9a381093c0c466136a0b72ba1954b050ccc89d
Aleks <G00396018@atu.ie>
2023-11-15 09:28:30 +0000

    Setup

---
+++ src/main/java/ie/atu/lab5springvalidation/Lab5SpringValidationApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab5springvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab5SpringValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab5SpringValidationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab5springvalidation/Lab5SpringValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab5springvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab5SpringValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
